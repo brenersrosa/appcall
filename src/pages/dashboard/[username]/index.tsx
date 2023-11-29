240e6c0f85b78c74e96b51f791cd9040ee3b3830
Brener Rosa <brenersrosa@gmail.com>
2023-11-29 00:37:08 -0300

    review: review code

--- src/pages/dashboard/[username]/index.tsx
+++ src/pages/dashboard/[username]/index.tsx
@@ -1,59 +1,10 @@
-import { useState } from 'react'
 import { GetServerSideProps } from 'next'
-import { useRouter } from 'next/router'
 import { getSession } from 'next-auth/react'
-import { useQuery } from '@tanstack/react-query'
-import dayjs from 'dayjs'
 
 import { Header } from '@/components/header'
-import { Calendar } from '@/components/ui/Calendar'
-
-import { api } from '@/lib/axios'
 import DashboardForm from './DashboardForm'
 
-interface Appointment {
-  id: string
-  date: string
-  name: string
-  email: string
-  phone: string
-  observations: string
-  created_at: string
-  updated_at: string
-  user_id: string
-  creator_id: string
-}
-
-interface UpcomingAppointment {
-  appointments: Appointment[]
-}
-
 export default function Dashboard() {
-  const [selectedDate, setSelectedDate] = useState<Date | null>(null)
-
-  const router = useRouter()
-  const username = String(router.query.username)
-
-  const selectedDateWithoutTime = selectedDate
-    ? dayjs(selectedDate).format('YYYY-MM-DD')
-    : null
-
-  const { data: upcomingAppointments } = useQuery<UpcomingAppointment>(
-    ['upcomingAppointments', selectedDateWithoutTime],
-    async () => {
-      const response = await api.get(`/users/${username}/list-appointments`, {
-        params: {
-          date: selectedDateWithoutTime,
-        },
-      })
-
-      return response.data
-    },
-    {
-      enabled: !!selectedDateWithoutTime,
-    },
-  )
-
   return (
     <>
       <Header />
